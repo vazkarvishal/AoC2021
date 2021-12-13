58d7c50d830def004b02140e9fa6e5a40b5316b2
Vishal Vazkar <vishal.vazkar@MMT-MAC-4236.domain.name>
2021-12-13 21:46:58 +0530

    Adds logger and refactors part one into separate function

--- 01/main.py
+++ 01/main.py
@@ -1,14 +1,29 @@
-if __name__ == '__main__':
-    input_file = open("./input.txt", "r")
-    puzzle_input = list(input_file)
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def part_one(input_data):
+
     depth_increased_count = 0
-    for index, measurement in enumerate(puzzle_input):
+    for index, measurement in enumerate(input_data):
         if index != 0 and measurement != "":
-            previous_depth = int(puzzle_input[index-1])
+            previous_depth = int(input_data[index - 1])
             current_depth = int(measurement)
             if current_depth > previous_depth:
                 depth_increased_count += 1
             else:
-                print(f"{previous_depth} is greater than {current_depth}. Skipping")
+                logger.debug(f"{previous_depth} is greater than {current_depth}. Skipping")
+    return depth_increased_count
+
+
+if __name__ == '__main__':
+    input_file = open("./input.txt", "r")
+    puzzle_input = list(input_file)
+
+    part_one_result = part_one(puzzle_input)
+    logger.info(f"part one result is {part_one_result}")
+
     input_file.close()
-    print(depth_increased_count)
+
